3127f8b6b973d79b3cdb71d9e31f206d09c96cda
nicoz23 <ncoo6274@gmail.com>
2023-02-05 17:28:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Pages from './pages/Pages'
+import Category from './components/Category'
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
